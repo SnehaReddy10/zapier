bc4228b6c164bcbd0babe632248b09e9063e0157
Sneha Reddy <snehareddy9178@gmail.com>
2024-08-16 07:38:46 +0530

    handle exceptions in hook

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -5,14 +5,19 @@ const app = express();
 const prismaClient = new PrismaClient();
 
 app.post('/:userId/:zapId', async (req, res) => {
-  const { userId, zapId } = req.params;
+  try {
+    const { userId, zapId } = req.params;
 
-  const [zaps, zapRuns] = await prismaClient.$transaction([
-    prismaClient.zaps.create({ data: { zapId, userId } }),
-    prismaClient.zapRun.create({ data: { zapId, userId } }),
-  ]);
+    const [zaps, zapRuns] = await prismaClient.$transaction([
+      prismaClient.zaps.create({ data: { zapId, userId } }),
+      prismaClient.zapRun.create({ data: { zapId, userId } }),
+    ]);
 
-  res.json({ success: true });
+    res.json({ success: true });
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false });
+  }
 });
 
 app.listen(3001, () => console.log('Listening on port 3001'));

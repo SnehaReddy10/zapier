512865af4aaf49c890cc3b1e99fc1e5eeaa40aa8
Sneha Reddy <snehareddy9178@gmail.com>
2024-08-18 10:47:36 +0530

    add signup page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,15 @@
+import Banner from '@/components/Banner';
+import Hero from '@/components/Hero';
+import Navbar from '@/components/Navbar';
+
 export default function Home() {
-  return <div className="">hello</div>;
+  return (
+    <div className="">
+      <Banner />
+      <Navbar />
+      <div className="mx-44 my-10 flex justify-center text-center">
+        <Hero />
+      </div>
+    </div>
+  );
 }

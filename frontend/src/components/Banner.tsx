5146804dfe38cd08ff5fa622daadb432aecb92e7
Sneha Reddy <snehareddy9178@gmail.com>
2024-08-18 14:52:35 +0530

    add cursor-pointer on button hover

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -1,8 +1,16 @@
+import { ImCross } from 'react-icons/im';
+import { MdCancel } from 'react-icons/md';
+
 function Banner() {
   return (
-    <div className="flex justify-center text-xxs font-semibold py-1 w-full bg-[#1f3121] text-white">
+    <div className="flex items-center justify-center text-xxs font-semibold py-1 w-full bg-[#1f3121] text-white">
       Don't miss HubSpot Co-founder and CTO Dharmesh Shah at ZapConnect 2024.
-      <span className="font-bold underline">Claim your spot!</span>
+      <span className="font-bold underline hover:cursor-pointer">
+        Claim your spot!
+      </span>
+      <span className="absolute p-1 end-5 flex border-[1px] border-[#1f3121] hover:cursor-pointer hover:border-[1px] hover:border-white">
+        <ImCross size={10} />
+      </span>
     </div>
   );
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
     <div className="flex sticky w-full bg-[#fffdf9] top-0 overflow-y-auto justify-between px-4 border-b-[1px] py-2 border-gray-200">
       <div className="flex gap-1 items-center">
         {/* change to LOGO */}
-        <span className="font-bold px-4">_zapier</span>
+        <span className="font-bold px-4 hover:cursor-pointer">_zapier</span>
         <LinkButton text="Product" link="product" showDropDownIcon={true} />
         <LinkButton text="Solutions" link="solutions" showDropDownIcon={true} />
         <LinkButton text="Resources" link="resources" showDropDownIcon={true} />

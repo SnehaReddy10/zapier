b0cb951320fa4f898e95d467a09ca3e8115a2839
Sneha Reddy <snehareddy9178@gmail.com>
2024-08-22 22:24:01 +0530

    refactor: remove logs

--- frontend/src/components/buttons/AddButton.tsx
+++ frontend/src/components/buttons/AddButton.tsx
@@ -3,10 +3,7 @@ import React from 'react';
 function AddButton({ onClick }: { onClick: () => void }) {
   return (
     <div
-      onClick={() => {
-        console.log('click');
-        onClick();
-      }}
+      onClick={onClick}
       className="my-1 text-[#5140bf] hover:cursor-pointer w-5 h-5 flex justify-center items-center hover:text-white transition-all ease-linear hover:bg-[#5140bf] rounded-full"
     >
       +

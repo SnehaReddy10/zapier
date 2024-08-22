bc8123910fdcb8d9eb54b10f35befc0168192bd2
Sneha Reddy <snehareddy9178@gmail.com>
2024-08-22 22:24:34 +0530

    feat : show modal with available actions and triggers

--- frontend/src/app/editor/page.tsx
+++ frontend/src/app/editor/page.tsx
@@ -1,17 +1,14 @@
 'use client';
 
-import EditZap from '@/components/editor/EditZap';
 import { EditorAppbar } from '@/components/editor/EditorAppBar';
 import { EditorPublishBar } from '@/components/editor/EditorPublishBar';
 import { EditorSideBar } from '@/components/editor/EditorSideBar';
 import EditorZap from '@/components/editor/EditorZap';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { ZapCellType } from '@/types/Zap';
 import Modal from '@/components/Modal';
 
 function Editor() {
-  const [modalType, setModalType] = useState<null | ZapCellType>(null);
-
   return (
     <div className={`relative h-screen flex justify-center items-center`}>
       <div className={`w-full h-full`}>
@@ -21,52 +18,72 @@ function Editor() {
           <div className="w-full">
             <EditorPublishBar />
             <div className="flex">
-              <Edit onClick={setModalType} />
-              <EditZap />
+              <Edit />
+              {/* <EditZap /> */}
             </div>
           </div>
         </div>
       </div>
-      {modalType != null && (
-        <div
-          onClick={() => setModalType((m) => null)}
-          className="absolute flex justify-center items-center z-10 w-full h-full opacity-45 bg-[#555655]"
-        >
-          <Modal type={modalType} />
-        </div>
-      )}
     </div>
   );
 }
 
-export function Edit({ onClick }: { onClick: (type: any) => void }) {
-  const [trigger, setTrigger] = useState<null | string>(null);
-  const [actions, setActions] = useState<any>([]);
-
-  useEffect(() => {}, []);
+export function Edit() {
+  const [trigger, setTrigger] = useState<any>(null);
+  const [currentIndex, setCurrentIndex] = useState<number>(1);
+  const [actions, setActions] = useState<any>([{ id: currentIndex }]);
+  const [modalType, setModalType] = useState<null | ZapCellType>(null);
 
   const addAction = () => {
-    console.log('add action');
-    setActions((x: any) => [...x, {}]);
+    setActions((x: any) => [
+      ...x,
+      { index: currentIndex + 1, interval: '2 mins' },
+    ]);
+  };
+
+  const selectAction = (action: any) => {
+    setActions((x: any) =>
+      x.map((x: any) => (x.index == action.index ? action : x))
+    );
   };
 
   return (
     <div className="w-full cursor-grab h-screen bg-[#f7f5f2] flex flex-col gap-1 items-center pt-10">
       <EditorZap
+        trigger={trigger}
         addAction={addAction}
         type={ZapCellType.trigger}
         index={1}
-        onClick={onClick}
+        onClick={(e) => {
+          setCurrentIndex(1);
+          setModalType(e);
+        }}
       />
       {actions.map((x: any, index: number) => (
         <EditorZap
-          onClick={onClick}
-          action={x.action}
+          key={x.id}
+          onClick={(e) => {
+            setCurrentIndex(x.index);
+            setModalType(e);
+          }}
+          action={x}
           type={ZapCellType.action}
-          index={index}
+          index={index + 1}
           addAction={addAction}
         />
       ))}
+      {modalType != null && (
+        <div
+          onClick={() => setModalType((m) => null)}
+          className="absolute inset-0 flex justify-center items-center z-10 w-full h-full bg-opacity-45 bg-[#555655]"
+        >
+          <Modal
+            index={currentIndex}
+            type={modalType}
+            onSelect={selectAction}
+          />
+        </div>
+      )}
     </div>
   );
 }

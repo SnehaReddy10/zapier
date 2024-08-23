6f6ee13685a58f36011f5903bb8bb5a4d47cfc2c
Sneha Reddy <snehareddy9178@gmail.com>
2024-08-23 07:50:55 +0530

    feat: open editPage sidebar on action or trigger select

--- frontend/src/app/editor/page.tsx
+++ frontend/src/app/editor/page.tsx
@@ -7,6 +7,7 @@ import EditorZap from '@/components/editor/EditorZap';
 import { useState } from 'react';
 import { ZapCellType } from '@/types/Zap';
 import Modal from '@/components/Modal';
+import EditZap from '@/components/editor/EditZap';
 
 function Editor() {
   return (
@@ -17,10 +18,7 @@ function Editor() {
           <EditorSideBar />
           <div className="w-full">
             <EditorPublishBar />
-            <div className="flex">
-              <Edit />
-              {/* <EditZap /> */}
-            </div>
+            <Edit />
           </div>
         </div>
       </div>
@@ -50,44 +48,48 @@ export function Edit() {
   };
 
   return (
-    <div className="w-full cursor-grab h-screen bg-[#f7f5f2] flex flex-col gap-1 items-center pt-10">
-      <EditorZap
-        trigger={trigger}
-        addAction={addAction}
-        type={ZapCellType.trigger}
-        index={1}
-        onClick={(e) => {
-          setCurrentIndex(1);
-          setModalType(e);
-        }}
-      />
-      {actions.map((x: any, index: number) => (
+    <div className="flex">
+      <div className="w-full cursor-grab h-screen bg-[#f7f5f2] flex flex-col gap-1 items-center pt-10">
         <EditorZap
-          key={x.id}
+          trigger={trigger}
+          addAction={addAction}
+          type={ZapCellType.trigger}
+          index={1}
           onClick={(e) => {
-            setCurrentIndex(x.index);
+            setCurrentIndex(1);
             setModalType(e);
           }}
-          action={x}
-          type={ZapCellType.action}
-          index={index + 1}
-          addAction={addAction}
         />
-      ))}
-      {modalType != null && (
-        <div
-          onClick={() => setModalType((m) => null)}
-          className="absolute inset-0 flex justify-center items-center z-10 w-full h-full bg-opacity-45 bg-[#555655]"
-        >
-          <Modal
-            index={currentIndex ?? 1}
-            type={modalType}
-            onSelect={
-              modalType == ZapCellType.action ? selectAction : selectTrigger
-            }
+        {actions.map((x: any, index: number) => (
+          <EditorZap
+            key={x.id}
+            onClick={(e) => {
+              setCurrentIndex(x.index);
+              setModalType(e);
+            }}
+            action={x}
+            type={ZapCellType.action}
+            index={index + 1}
+            addAction={addAction}
           />
-        </div>
-      )}
+        ))}
+        {modalType != null && (
+          <div
+            onClick={() => setModalType((m) => null)}
+            className="absolute inset-0 flex justify-center items-center z-10 w-full h-full bg-opacity-45 bg-[#555655]"
+          >
+            <Modal
+              index={currentIndex ?? 1}
+              type={modalType}
+              onSelect={
+                modalType == ZapCellType.action ? selectAction : selectTrigger
+              }
+            />
+          </div>
+        )}
+      </div>
+
+      {(trigger || actions[currentIndex - 1].action) && <EditZap />}
     </div>
   );
 }

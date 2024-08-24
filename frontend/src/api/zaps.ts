34d3cb1c8324b84a331f322185ef7e8a380c3277
Sneha Reddy <snehareddy9178@gmail.com>
2024-08-24 22:16:11 +0530

    feat: add create zap api utility

---
+++ frontend/src/api/zaps.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { BACKEND_URL } from '../../config';
+import { TOKEN } from '@/constants';
+import { CreateZapMapper } from '@/mapper/create-zap.mapper';
+
+export async function createZap({ actions, trigger }: any) {
+  try {
+    const zapRequest = CreateZapMapper({ actions, trigger });
+    console.log(zapRequest);
+    const result = await axios.post(`${BACKEND_URL}/zap`, zapRequest, {
+      headers: { Authorization: `Bearer ${localStorage.getItem(TOKEN)}` },
+    });
+    console.log('result', result);
+    return result.data;
+  } catch (err: any) {
+    console.log(err.response.data.error);
+    return { success: false, error: err.response.data.error };
+  }
+}

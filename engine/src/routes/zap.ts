9c39b45442cf73237d0b890a0a736b86bb7bf343
Sneha Reddy <snehareddy9178@gmail.com>
2024-08-24 18:14:21 +0530

    fix: create zap route

--- engine/src/routes/zap.ts
+++ engine/src/routes/zap.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import { authMiddleware } from '../middleware/auth';
 import { GENERIC } from '../constants/error-messages';
+import { STATUS_CODES } from '../constants/status-codes';
 
 export const zapRouter = express.Router();
 const prismaClient = new PrismaClient();
@@ -11,12 +12,32 @@ zapRouter.post('', async (req, res) => {
     const { trigger, actions } = req.body;
 
     const zap = await prismaClient.zap.create({
-      data: { trigger, actions },
+      data: {
+        trigger: {
+          create: {
+            availableTriggerId: trigger.availableTriggerId,
+            eventId: trigger.eventId,
+          },
+        },
+        actions: {
+          create: actions.map((x: any) => ({
+            availableActionId: x.availableActionId,
+            eventId: x.eventId,
+          })),
+        },
+      },
+      select: {
+        trigger: { select: { trigger: true } },
+        actions: { select: { action: true } },
+      },
     });
+
     res.json({ success: true, zap });
   } catch (err) {
     console.log('Create Zap Failed', err);
-    return res.json({ success: false, message: GENERIC.ServiceUnavailable });
+    return res
+      .status(STATUS_CODES.ServiceUnavailable)
+      .json({ success: false, message: GENERIC.ServiceUnavailable });
   }
 });
 

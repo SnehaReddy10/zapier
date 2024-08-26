7c5c402281dda8483ce4b7b4056783d358ae1c48
Sneha Reddy <snehareddy9178@gmail.com>
2024-08-26 21:13:36 +0530

    refactor code

--- frontend/src/app/editor/page.tsx
+++ frontend/src/app/editor/page.tsx
@@ -3,12 +3,8 @@
 import { EditorAppbar } from '@/components/editor/EditorAppBar';
 import { EditorPublishBar } from '@/components/editor/EditorPublishBar';
 import { EditorSideBar } from '@/components/editor/EditorSideBar';
-import EditorZap from '@/components/editor/EditorZap';
-import { useEffect, useState } from 'react';
-import { ZapCellType } from '@/types/Zap';
-import Modal from '@/components/Modal';
-import EditZap from '@/components/editor/EditZap';
-import { createZap } from '@/api/zaps';
+import { useState } from 'react';
+import { Edit } from '@/components/editor/Edit';
 
 function Editor() {
   const [canPublish, setCanPublish] = useState(false);
@@ -24,123 +20,6 @@ function Editor() {
           <Edit setCanPublish={setCanPublish} publish={publish} />
         </div>
       </div>
-    </div>
-  );
-}
-
-export function Edit({ setCanPublish, publish }: any) {
-  const [trigger, setTrigger] = useState<any>(null);
-  const [currentIndex, setCurrentIndex] = useState<number>(1);
-  const [actions, setActions] = useState<any>([{ index: currentIndex + 1 }]);
-  const [modalType, setModalType] = useState<null | ZapCellType>(null);
-  const [showEditZap, setShowEditZap] = useState(false);
-
-  useEffect(() => {
-    if (
-      trigger?.eventId &&
-      actions.every((x: any) => typeof x.eventId != typeof undefined)
-    ) {
-      setCanPublish(true);
-    } else {
-      setCanPublish(false);
-    }
-
-    if (publish) {
-      createZap({ actions, trigger });
-    }
-  }, [trigger, actions, publish]);
-
-  const addAction = (index: number) => {
-    setCurrentIndex(index + 1);
-    setActions((x: any) => [...x, { index: index + 1, interval: '2 mins' }]);
-  };
-
-  const selectZap = (zap: any, zapType: ZapCellType) => {
-    if (zapType == ZapCellType.trigger) {
-      setTrigger(() => zap);
-    } else if (zapType == ZapCellType.action) {
-      setActions((x: any) =>
-        x.map((x: any) => (x.index == zap.index ? zap : x))
-      );
-    }
-  };
-
-  const selectEvent = (event: any, zapType?: ZapCellType) => {
-    if (zapType == ZapCellType.trigger) {
-      setTrigger({ ...trigger, eventName: event.name, eventId: event.id });
-    } else if (zapType == ZapCellType.action) {
-      setActions((x: any) =>
-        x.map((action: any, index: number) => {
-          if (action.index == currentIndex) {
-            return { ...action, eventName: event.name, eventId: event.id };
-          }
-          return action;
-        })
-      );
-    }
-  };
-
-  return (
-    <div className="flex flex-grow max-h-screen">
-      <div className="w-full min-h-screen scroll-smooth overflow-y-scroll py-14 no-scrollbar cursor-grab bg-gray-50 flex flex-col gap-1 items-center pt-10">
-        <EditorZap
-          trigger={trigger}
-          addAction={addAction}
-          type={ZapCellType.trigger}
-          index={1}
-          onClick={(e) => {
-            if (trigger) {
-              setShowEditZap(true);
-            } else {
-              setModalType(e);
-            }
-            setCurrentIndex(1);
-          }}
-        />
-        {actions.map((x: any, index: number) => (
-          <EditorZap
-            key={x.id}
-            onClick={(e) => {
-              if (x.action) {
-                setShowEditZap(true);
-              } else {
-                setModalType(e);
-              }
-              setCurrentIndex(x.index);
-            }}
-            action={x}
-            type={ZapCellType.action}
-            index={index + 1}
-            addAction={addAction}
-          />
-        ))}
-        {modalType != null && (
-          <div
-            onClick={() => setModalType((m) => null)}
-            className="absolute inset-0 flex justify-center items-center z-10 w-full h-full bg-opacity-45 bg-[#555655]"
-          >
-            <Modal
-              index={currentIndex ?? 1}
-              type={modalType}
-              onSelect={selectZap}
-            />
-          </div>
-        )}
-      </div>
-
-      {showEditZap && (
-        <EditZap
-          setShowModal={setModalType}
-          setShowEditZap={setShowEditZap}
-          zapType={currentIndex == 1 ? ZapCellType.trigger : ZapCellType.action}
-          onEventSelect={selectEvent}
-          zap={
-            currentIndex == 1
-              ? trigger
-              : actions.find((x: any) => x.index == currentIndex)
-          }
-        />
-      )}
     </div>
   );
 }

c875aa0f13640cb32d500891b57b2af24a314644
Sneha Reddy <snehareddy9178@gmail.com>
2024-08-26 22:06:17 +0530

    feat: add events to available actions

--- engine/src/routes/admin.ts
+++ engine/src/routes/admin.ts
@@ -1,34 +1,62 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import { STATUS_CODES } from '../constants/status-codes';
+import { GENERIC } from '../constants/error-messages';
 
 export const adminRouter = express.Router();
 const prismaClient = new PrismaClient();
 
 adminRouter.post('/availableTrigger', async (req, res) => {
-  const { trigger, events, metaData } = req.body;
+  try {
+    const { trigger, events, metaData } = req.body;
 
-  const availableTrigger = await prismaClient.availableTriggers.create({
-    data: { trigger, metaData },
-  });
-  events.map(async (event: any) => {
-    await prismaClient.event.create({
-      data: { name: event.name, availableTriggersId: availableTrigger.id },
+    const availableTrigger = await prismaClient.availableTriggers.create({
+      data: { trigger, metaData },
     });
-  });
 
-  const newTrigger = await prismaClient.availableTriggers.findFirst({
-    where: { id: availableTrigger.id },
-    select: { trigger: true, events: true },
-  });
+    events.map(async (event: any) => {
+      await prismaClient.event.create({
+        data: { name: event.name, availableTriggersId: availableTrigger.id },
+      });
+    });
 
-  res.json({ success: true, availableTrigger, event: newTrigger });
+    const newTrigger = await prismaClient.availableTriggers.findFirst({
+      where: { id: availableTrigger.id },
+      select: { trigger: true, events: true },
+    });
+
+    res.json({ success: true, availableTrigger, event: newTrigger });
+  } catch (err) {
+    console.log('Add Available Trigger Failed', err);
+    return res
+      .status(STATUS_CODES.ServiceUnavailable)
+      .json({ success: false, message: GENERIC.ServiceUnavailable });
+  }
 });
 
 adminRouter.post('/availableAction', async (req, res) => {
-  const { action, metaData } = req.body;
+  try {
+    const { action, events, metaData } = req.body;
 
-  const availableAction = await prismaClient.availableActions.create({
-    data: { action, metaData },
-  });
-  res.json({ success: true, availableAction });
+    const availableAction = await prismaClient.availableActions.create({
+      data: { action, metaData },
+    });
+
+    events.map(async (event: any) => {
+      await prismaClient.event.create({
+        data: { name: event.name, availableActionsId: availableAction.id },
+      });
+    });
+
+    const newTrigger = await prismaClient.availableActions.findFirst({
+      where: { id: availableAction.id },
+      select: { action: true, events: true },
+    });
+    res.json({ success: true, availableAction, event: newTrigger });
+  } catch (err) {
+    console.log('Add Available Action Failed', err);
+    return res
+      .status(STATUS_CODES.ServiceUnavailable)
+      .json({ success: false, message: GENERIC.ServiceUnavailable });
+  }
 });

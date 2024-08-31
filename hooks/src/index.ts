acfc54fa29866a34b7d9bc7102ac1dd719a77b4c
Sneha Reddy <snehareddy9178@gmail.com>
2024-08-31 16:39:08 +0530

    add fin-new-records and test-trigger routes

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,8 +1,35 @@
 import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
 import express from 'express';
 
+declare module 'express-serve-static-core' {
+  interface Request {
+    userId: string;
+  }
+}
+
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 const prismaClient = new PrismaClient();
+
+app.post('/test-trigger/:triggerId', async (req, res) => {
+  const payload = req.body;
+
+  await prismaClient.hookReq.create({
+    data: { payload: JSON.stringify(payload), triggerId: req.params.triggerId },
+  });
+
+  return res.json({ success: true });
+});
+
+app.get('/find-new-records/:triggerId', async (req, res) => {
+  const records = await prismaClient.hookReq.findMany({
+    where: { triggerId: req.params.triggerId },
+  });
+  return res.json({ success: true, records });
+});
 
 app.post('/:userId/:zapId', async (req, res) => {
   try {

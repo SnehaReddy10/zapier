f2ffb95b504c284f32c870c6a48737f889324eaa
Sneha Reddy <snehareddy9178@gmail.com>
2024-08-31 16:39:53 +0530

    add test-trigger and find-new-records api call

--- frontend/src/api/triggers.ts
+++ frontend/src/api/triggers.ts
@@ -1,12 +1,15 @@
 import { TOKEN } from '@/constants';
 import axios from 'axios';
-import { BACKEND_URL } from '../../config';
+import { BACKEND_ENGINE_URL, BACKEND_HOOKS_URL } from '../../config';
 
 export async function getAvailableTriggers() {
   try {
-    const result = await axios.get(`${BACKEND_URL}/trigger/availableTriggers`, {
-      headers: { Authorization: `Bearer ${localStorage.getItem(TOKEN)}` },
-    });
+    const result = await axios.get(
+      `${BACKEND_ENGINE_URL}/trigger/availableTriggers`,
+      {
+        headers: { Authorization: `Bearer ${localStorage.getItem(TOKEN)}` },
+      }
+    );
     return result.data;
   } catch (err: any) {
     console.log(err.response.data.error);
@@ -17,11 +20,42 @@ export async function getAvailableTriggers() {
 export async function getAvailableEventsForTrigger(triggerId: string) {
   try {
     const result = await axios.get(
-      `${BACKEND_URL}/trigger/events/${triggerId}`,
+      `${BACKEND_ENGINE_URL}/trigger/events/${triggerId}`,
       {
         headers: { Authorization: `Bearer ${localStorage.getItem(TOKEN)}` },
       }
     );
+    return result.data;
+  } catch (err: any) {
+    console.log(err.response.data.error);
+    return { success: false, error: err.response.data.error };
+  }
+}
+
+export async function testTrigger(triggerId: string) {
+  try {
+    const result = await axios.get(
+      `${BACKEND_HOOKS_URL}/test-trigger/${triggerId}`,
+      {
+        headers: { Authorization: `Bearer ${localStorage.getItem(TOKEN)}` },
+      }
+    );
+    return result.data;
+  } catch (err: any) {
+    console.log(err.response.data.error);
+    return { success: false, error: err.response.data.error };
+  }
+}
+
+export async function findNewRecords(triggerId: string) {
+  try {
+    const result = await axios.get(
+      `${BACKEND_HOOKS_URL}/find-new-records/${triggerId}`,
+      {
+        headers: { Authorization: `Bearer ${localStorage.getItem(TOKEN)}` },
+      }
+    );
+    console.log({ result: result });
     return result.data;
   } catch (err: any) {
     console.log(err.response.data.error);
